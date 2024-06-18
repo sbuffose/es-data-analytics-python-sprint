fac226c7daf60c954fe740e3ff31cd18ccb42ab6
Sebas Buffo Sempe <sebastianbuffosempe@gmail.com>
2024-06-18 17:56:53 -0300

    Create sum3.py

---
+++ sum3.py
@@ -0,0 +1,11 @@
+def sum3(first_number, second_number, third_number):
+    """
+    Toma tres números y devuelve la suma de esos números.
+    """
+    return first_number + second_number + third_number
+
+
+print(sum3(0, 0, 0))
+print(sum3(1, 2, 3))
+print(sum3(4, 5, 6))
+print(sum3(-1, 1, 0))
